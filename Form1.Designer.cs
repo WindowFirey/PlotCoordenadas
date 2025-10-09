35ca73c353d6747fd9293db40372b5cb3d3b56e6
Felipe Matsumoto <felipematsumoto007@gmail.com>
2025-10-09 12:04:43 -0300

    commit 2025/10/09

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace PlotCoordenadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Obter Inormações";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(12, 163);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1880, 866);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(12, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 261);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(2, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "De qual tabela deseja carregar o mapa";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(2, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clientes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button3.Location = new System.Drawing.Point(2, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Lotes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button4.Location = new System.Drawing.Point(2, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(245, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Coletados dos Clientes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button5.Location = new System.Drawing.Point(2, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(245, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Rede de Esgoto";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button6.Location = new System.Drawing.Point(2, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(245, 30);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Rede Fluvial";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button7.Location = new System.Drawing.Point(2, 219);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(245, 30);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Registros";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Plot de Coordenadas Maps";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlotCoordenadas
+{
+    public partial class Form1 : Form
+    {
+        private void enableDisanbleObject(object objeto, bool status)
+        {
+            objeto.GetType().GetProperty("Enabled").SetValue(objeto, status);
+            objeto.GetType().GetProperty("Visible").SetValue(objeto, status);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //btn Obter Informacoes
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, true);
+        }
+
+
+
+        //btn Clientes
+        private void button2_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+
+        }
+
+        //btn Lotes
+        private void button3_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+        }
+
+        //btn Dados Clientes
+        private void button4_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+        }
+
+        //btn Rede Esgoto
+        private void button5_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+        }
+
+        //btn Rede Fluvial
+        private void button6_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+        }
+
+        //btn Registros
+        private void button7_Click(object sender, EventArgs e)
+        {
+            enableDisanbleObject(panel1, false);
+        }
+    }
+}
